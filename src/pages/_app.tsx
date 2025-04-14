152cf7e05afb78fa6155d99cfee2ab27b21b8fbe
Rafael Brito <rafael.brito.1422@gmail.com>
2025-04-14 16:14:59 -0300

    feat: image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,17 @@
 import type { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
 
+globalStyles();
+import logoImg from "../assets/Logo.svg";
+import { Container, Header } from "../styles/pages/app";
+import Image from "next/image";
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="Ignite Shop" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
